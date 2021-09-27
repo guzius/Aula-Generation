585677d9a835f8411d025ed80141355771c06ada
Guzius <gustavotaques11@gmail.com>
2021-09-27 17:05:51 -0300

    Versão 1 do Blog Pessoal

---
+++ Blog Pessoal/src/main/java/br/org/generation/blogpessoal/PessoalBlogApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoalBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoalBlogApplication.class, args);
+	}
+
+}
